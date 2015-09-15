95d77930c67dee4dc40b364435894e215394980c
Alex Michael <hi@alexmic.net>
2015-09-16 01:30:54 +0200

    Return a random URL from boss which has both sides > minimum_side

--- ga/boss.py
+++ ga/boss.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import random
 from requests_oauthlib import OAuth1Session
 from ga import settings
 
@@ -12,16 +13,16 @@ class BossImage(object):
         self.height = int(obj['height'])
 
 
-def image_url_for_term(term, minimum_width=settings.IMAGE_WIDTH):
+def image_url_for_term(term, minimum_side=settings.IMAGE_SIDE):
     boss = OAuth1Session(settings.BOSS_KEY, client_secret=settings.BOSS_SECRET)
 
     params = {'q': term, 'dimensions': 'large'}
     response = boss.get(settings.BOSS_IMAGE_URL, params=params).json()
     results = response['bossresponse']['images']['results']
 
-    for result in results:
-        img = BossImage(result)
-        if img.width >= minimum_width:
-            return img.url
+    def is_valid(i):
+        return i.width >= minimum_side and i.height >= minimum_side
 
-    return None
+    images = [BossImage(r) for r in results]
+    choice = random.choice([i for i in images if is_valid(i)])
+    return choice.url if choice else None
