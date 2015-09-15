08d19bb7806dda23c99139635565696ebb60d260
Alex Michael <hi@alexmic.net>
2015-09-16 01:34:36 +0200

    Make create view write to temp file for testing

--- ga/views.py
+++ ga/views.py
@@ -1,21 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template, redirect, url_for
+import shortuuid
 
 from ga import settings
 from ga.boss import image_url_for_term
-from ga.image import trumpify_image
+from ga.image import trumpify_image_from_url
 
 
 blueprint = Blueprint('views', __name__)
 
 
-@blueprint.route('/random')
-def random():
-    url = image_url_for_term(request.args.get('term'))
-    return 'ok'
+@blueprint.route('/')
+def index():
+    return render_template('index.html')
 
 
 @blueprint.route('/create', methods=['POST'])
-def create_image(self):
-    pass
+def create_image():
+    term = request.form['term']
+    url = image_url_for_term(term)
+    image = trumpify_image_from_url(url, term)
+    uuid = shortuuid.uuid()
+    image.save('/tmp/gen/%s.jpg' % uuid, 'JPEG')
+    return redirect('/i/%s' % uuid)
+
+
+@blueprint.route('/i/<uuid>')
+def view_image(uuid):
+    src = url_for('static', filename="gen/%s.jpg" % uuid)
+    return render_template('image.html', src=src)
