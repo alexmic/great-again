5a75a607a1493143dbb646ddb8ce9ffb2ca67acc
Alex Michael <hi@alexmic.net>
2015-09-16 23:34:26 +0200

    Count views and submissions

--- ga/views.py
+++ ga/views.py
@@ -2,7 +2,7 @@
 
 from flask import Blueprint, request, render_template, redirect, abort
 
-from ga.cache import cache
+from ga import cache
 from ga.boss import get_pil_image_for_term
 from ga.image.draw import trumpify_image_from_url
 from ga.image.store import upload_image_from_pil_image
@@ -12,7 +12,7 @@ from ga.utils import timing
 blueprint = Blueprint('views', __name__)
 
 
-@cache(prefix='term', postfix='image')
+@cache.memoize(prefix='image')
 def get_trumpified_image_url_for_term(term):
     term = term.lower()
     with timing('get_pil_image_for_term'):
@@ -30,13 +30,15 @@ def index():
     return render_template('index.html')
 
 
-@blueprint.route('/redirect', methods=['POST'])
-def redirect_for_term():
+@blueprint.route('/submit', methods=['POST'])
+def submit():
     term = request.form['term']
+    cache.incr(term, prefix='submissions')
     return redirect('/' + term)
 
 
 @blueprint.route('/<term>')
-def view_image(term):
+def view(term):
     url = get_trumpified_image_url_for_term(term)
+    cache.incr(term, prefix='views')
     return render_template('image.html', src=url)
