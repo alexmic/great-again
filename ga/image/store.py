9c12e2d6890a32d93ea6b2a9ae6f000d46182377
Alex Michael <hi@alexmic.net>
2015-09-20 12:47:55 +0200

    Move S3 bucket connection into `upload_image`

--- ga/image/store.py
+++ ga/image/store.py
@@ -8,9 +8,6 @@ from boto.s3.connection import S3Connection
 from ga import settings
 
 
-conn = S3Connection(settings.AWS_KEY, settings.AWS_SECRET)
-bucket = conn.get_bucket(settings.AWS_BUCKET)
-
 def upload_image_from_pil_image(image):
     output = StringIO.StringIO()
     image.save(output, 'JPEG')
@@ -19,9 +16,12 @@ def upload_image_from_pil_image(image):
 
 
 def upload_image(stream):
-    uuid = shortuuid.uuid()
-    key = bucket.new_key(uuid + '.jpg')
+    conn = S3Connection(settings.AWS_KEY, settings.AWS_SECRET)
+    bucket = conn.get_bucket(settings.AWS_BUCKET)
+
+    key = bucket.new_key(shortuuid.uuid() + '.jpg')
     key.set_contents_from_string(stream.getvalue())
     key.set_metadata('Content-Type', 'image/jpeg')
     key.set_acl('public-read')
+
     return key.generate_url(expires_in=0, query_auth=False, force_http=True)
